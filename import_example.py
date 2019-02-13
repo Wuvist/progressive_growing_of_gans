096fbcf205cd0328c0b9a433c1885de3188d9025
Wuvist <wuvist@gmail.com>
2019-02-13 22:38:26 +0800

    test

--- import_example.py
+++ import_example.py
@@ -12,18 +12,43 @@ with open('karras2018iclr-celebahq-1024x1024.pkl', 'rb') as file:
 
 # Generate latent vectors.
 latents = np.random.RandomState(1000).randn(1000, *Gs.input_shapes[0][1:]) # 1000 random latents
-latents = latents[[477, 56, 83, 887, 583, 391, 86, 340, 341, 415]] # hand-picked top-10
+latents = latents[[477]] # hand-picked top-10
 
 # Generate dummy labels (not used by the official networks).
 labels = np.zeros([latents.shape[0]] + Gs.input_shapes[1][1:])
 
 # # Run the generator to produce a set of images.
-images = Gs.run(latents, labels)
+# images = Gs.run(latents, labels)
 
-# Convert images to PIL-compatible format.
+# # Convert images to PIL-compatible format.
+# images = np.clip(np.rint((images + 1.0) / 2.0 * 255.0), 0.0, 255.0).astype(np.uint8) # [-1,1] => [0,255]
+# images = images.transpose(0, 2, 3, 1) # NCHW => NHWC
+
+# # Save images as PNG.
+# for idx in range(images.shape[0]):
+#     PIL.Image.fromarray(images[idx], 'RGB').save('img%d.png' % idx)
+
+def load_image( infilename ) :
+    img = PIL.Image.open( infilename )
+    img.load()
+    data = np.asarray(img, dtype="float32")
+    return data
+
+img = load_image("ww.jpg")
+img = img.reshape(1, 1024, 1024, 3)
+img = tf.transpose(img, [0, 3, 1, 2]).eval()
+print(img.shape)
+# img = img.transpose(3, 1, 2)
+history = Gs.reverse_gan_for_etalons(latents, labels, img)
+
+
+data = history[-1][1]
+
+images = Gs.run(data, labels)
+
 images = np.clip(np.rint((images + 1.0) / 2.0 * 255.0), 0.0, 255.0).astype(np.uint8) # [-1,1] => [0,255]
 images = images.transpose(0, 2, 3, 1) # NCHW => NHWC
 
 # Save images as PNG.
 for idx in range(images.shape[0]):
-    PIL.Image.fromarray(images[idx], 'RGB').save('img%d.png' % idx)
+    PIL.Image.fromarray(images[idx], 'RGB').save('i%d.png' % idx)
